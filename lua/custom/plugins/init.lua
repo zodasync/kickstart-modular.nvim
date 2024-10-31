026eb848bb15f7cc76e72d9326bdca330c5b3bea
Zane Tedder <zane.tedder@wvm.edu>
2024-10-31 15:52:17 -0700

    update : catppuccin mods

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,18 +3,14 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'catppuccin/nvim',
-    lazy = false,
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      require('catppuccin').setup()
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 }
