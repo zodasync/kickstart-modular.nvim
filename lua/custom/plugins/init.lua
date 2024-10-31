0b322aadf85656df6451c729e0e88ccf556fc18d
Zane Tedder <zane.tedder@wvm.edu>
2024-10-31 15:38:58 -0700

    update : gitsigns stuff done

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,14 +9,12 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
+    lazy = false,
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin'
-
-      -- You can configure highlights by doing something like:
-      --vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
