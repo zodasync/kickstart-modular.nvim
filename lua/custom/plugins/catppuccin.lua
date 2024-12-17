3537c878ceb0220df50bbd840fa11f4ed5c8731d
Zane Tedder <zane.tedder@wvm.edu>
2024-12-17 15:50:24 -0800

    tweaked catppuccin.lua to set background to transparent to allow terminal to pick up the slack

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -11,6 +11,15 @@ return {
     config = function()
       -- Load the colorscheme here.
       vim.cmd.colorscheme 'catppuccin-frappe'
+      -- transparent bg stuff (catppuccin theme already in play at terminal level)
+      vim.opt.background = 'dark' -- or 'light'
+      vim.cmd [[
+          highlight Normal guibg=NONE ctermbg=NONE
+          highlight NonText guibg=NONE ctermbg=NONE
+          highlight LineNr guibg=NONE ctermbg=NONE
+          highlight SignColumn guibg=NONE ctermbg=NONE
+          highlight EndOfBuffer guibg=NONE ctermbg=NONE
+      ]]
     end,
   },
 }
